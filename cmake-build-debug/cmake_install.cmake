5df7ca2a8c540f5cefdc2bf45650d3228c033c71
bengnk <ben.gnielka@icloud.com>
2025-02-26 14:44:59 +0100

    2.0

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Administrator/OneDrive - HWR Berlin/Dokumente/HWR/Roulett
+# Install script for directory: C:/Users/Administrator/Roulette
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Roulett")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/GameC")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Administrator/OneDrive - HWR Berlin/Dokumente/HWR/Roulett/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Administrator/Roulette/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
